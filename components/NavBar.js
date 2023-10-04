c506a7bfe56fe8202d47230522ed074b1071b816
kjm541006 <kjm541113@gmail.com>
2023-10-04 17:49:38 +0900

    nextjs css module 사용하기로 결정

--- components/NavBar.js
+++ components/NavBar.js
@@ -5,36 +5,36 @@ import styles from "./NavBar.module.css";
 const NavBar = () => {
   const router = useRouter();
   return (
-    // <nav className={styles.nav}>
-    //    <Link href="/" className={`${styles.link} ${router.pathname === "/" && styles.active}`}>
-    //     Home
-    //   </Link>
-    //   <Link href="/about" className={`${styles.link} ${router.pathname === "/about" && styles.active}`}>
-    //     About
-    //   </Link>
-    // </nav>
-    <nav className="nav">
-      <Link href="/" className={router.pathname === "/" ? "active" : ""}>
-        <span>Home</span>
+    <nav className={styles.nav}>
+      <Link href="/" className={`${styles.link} ${router.pathname === "/" && styles.active}`}>
+        Home
       </Link>
-      <Link href="/about" className={router.pathname === "/about" ? "active" : ""}>
-        <span>About</span>
+      <Link href="/about" className={`${styles.link} ${router.pathname === "/about" && styles.active}`}>
+        About
       </Link>
-      <style jsx>
-        {`
-          nav {
-            background-color: tomato;
-          }
-          span {
-            text-decoration: none;
-            color: pink;
-          }
-          .active {
-            color: blue;
-          }
-        `}
-      </style>
     </nav>
+    // <nav className="nav">
+    //   <Link href="/" className={router.pathname === "/" ? "active" : ""}>
+    //     <span>Home</span>
+    //   </Link>
+    //   <Link href="/about" className={router.pathname === "/about" ? "active" : ""}>
+    //     <span>About</span>
+    //   </Link>
+    //   <style jsx>
+    //     {`
+    //       nav {
+    //         background-color: tomato;
+    //       }
+    //       span {
+    //         text-decoration: none;
+    //         color: pink;
+    //       }
+    //       .active {
+    //         color: blue;
+    //       }
+    //     `}
+    //   </style>
+    // </nav>
   );
 };
 
